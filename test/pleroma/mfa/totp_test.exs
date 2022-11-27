fb7c6bed64b4db21b689ed69de0ac98b1f45aaae
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-26 20:24:54 -0500

    spelling: secret
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- test/pleroma/mfa/totp_test.exs
+++ test/pleroma/mfa/totp_test.exs
@@ -9,13 +9,13 @@ defmodule Pleroma.MFA.TOTPTest do
 
   test "create provisioning_uri to generate qrcode" do
     uri =
-      TOTP.provisioning_uri("test-secrcet", "test@example.com",
+      TOTP.provisioning_uri("test-secret", "test@example.com",
         issuer: "Plerome-42",
         digits: 8,
         period: 60
       )
 
     assert uri ==
-             "otpauth://totp/test@example.com?digits=8&issuer=Plerome-42&period=60&secret=test-secrcet"
+             "otpauth://totp/test@example.com?digits=8&issuer=Plerome-42&period=60&secret=test-secret"
   end
 end
